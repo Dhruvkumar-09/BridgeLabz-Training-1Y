d45dde4c908ba3d5f90a352cc1aebc3d7666b8b8
dhruvkumar1605-learner <dhruvkumar1605@gmail.com>
2026-01-13 10:04:22 +0530

    Added solutions of the given assingment problems

---
+++ basic-programs/KmTOMiles.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KmTOMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double km = sc.nextDouble();
+
+        double miles = km * 0.621371;
+
+        System.out.println(km + " kilometers = " + miles + " miles");
+
+        sc.close();
+    }
+}
